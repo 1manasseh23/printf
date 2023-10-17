51284e9df3fa7ab905713b0da4995be7188aa4ef
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-10-17 10:04:54 -0700

    main.h file

--- main.h
+++ main.h
@@ -5,4 +5,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int str_char_printf(const char *format, ...);
 int id_printf(const char *format, ...);
+int print_str_escaped(char *str);
+int s_printf(const char *format, ...);
 #endif
